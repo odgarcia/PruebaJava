3a076ec73fa721b74fa8db3b41aa981c9de7ac17
Oscar Garcia <od.garcia@uniandes.edu.co>
2018-10-07 19:36:02 -0500

    Scripts SQL

---
+++ DDL.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE BeitechTestBD;
+CREATE USER user_beitech WITH ENCRYPTED PASSWORD 'beitech123*';
+GRANT ALL PRIVILEGES ON DATABASE BeitechTestBD TO user_beitech;
+
+-- Drop table
+
+-- DROP TABLE public.customer
+
+CREATE TABLE public.customer (
+	id serial NOT NULL,
+	"name" varchar(255) NULL,
+	email varchar(255) NULL,
+	CONSTRAINT customer_pkey PRIMARY KEY (id)
+);
+
+-- Drop table
+
+-- DROP TABLE public.customer_available_product
+
+CREATE TABLE public.customer_available_product (
+	id serial NOT NULL,
+	customer_id int4 NULL,
+	product_id int4 NULL,
+	CONSTRAINT "CustomerAvailableProduct_pkey" PRIMARY KEY (id),
+	CONSTRAINT customer_fk FOREIGN KEY (customer_id) REFERENCES customer(id),
+	CONSTRAINT product_fk FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+-- Drop table
+
+-- DROP TABLE public.customer_order
+
+CREATE TABLE public.customer_order (
+	id serial NOT NULL,
+	order_date date NULL DEFAULT 'now'::text::date,
+	total float8 NULL,
+	delivery_address varchar(255) NULL,
+	customer_id int4 NULL,
+	CONSTRAINT customer_order_pkey PRIMARY KEY (id),
+	CONSTRAINT customer_fk FOREIGN KEY (customer_id) REFERENCES customer(id)
+);
+
+-- Drop table
+
+-- DROP TABLE public.order_detail
+
+CREATE TABLE public.order_detail (
+	id serial NOT NULL,
+	quantity int4 NULL,
+	price float8 NULL,
+	product_id int4 NULL,
+	order_id int4 NULL,
+	CONSTRAINT order_detail_pkey PRIMARY KEY (id),
+	CONSTRAINT order_fk FOREIGN KEY (order_id) REFERENCES customer_order(id),
+	CONSTRAINT product_fk FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+-- Drop table
+
+-- DROP TABLE public.product
+
+CREATE TABLE public.product (
+	id serial NOT NULL,
+	"name" varchar NOT NULL,
+	price float8 NOT NULL,
+	CONSTRAINT product_pkey PRIMARY KEY (id)
+);
+
+
+GRANT ALL ON TABLE public.customer_order TO user_beitech;
+GRANT ALL ON TABLE public.product TO user_beitech;
+GRANT ALL ON TABLE public.order_detail TO user_beitech;
+GRANT ALL ON TABLE public.customer_available_product TO user_beitech;
+GRANT ALL ON TABLE public.customer TO user_beitech;
---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.customer (id, "name", email) VALUES(1, 'Oscar Garcia', 'correofalso@corre.com');
+INSERT INTO public.customer (id, "name", email) VALUES(3, 'Jhon Smith', 'jhonsmith@corre.com');
+INSERT INTO public.customer (id, "name", email) VALUES(2, 'JazminAngulo', 'correofalse@corre.com');
+INSERT INTO public.product (id, "name", price) VALUES(1, 'Producto #1', 1275);
+INSERT INTO public.product (id, "name", price) VALUES(2, 'Producto #2', 1200);
+INSERT INTO public.product (id, "name", price) VALUES(3, 'Producto #3', 2000);
+INSERT INTO public.product (id, "name", price) VALUES(4, 'Producto #4', 5000);
+INSERT INTO public.product (id, "name", price) VALUES(5, 'Producto #5', 1000);
+INSERT INTO public.product (id, "name", price) VALUES(6, 'Producto #6', 10000);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(1, 1, 1);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(2, 1, 2);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(3, 1, 3);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(4, 2, 1);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(5, 2, 2);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(6, 3, 4);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(7, 3, 5);
+INSERT INTO public.customer_available_product (id, customer_id, product_id) VALUES(8, 3, 6);
